1521bfd5bdfc6ad321895ed661985f479648af53
binary64 <1680627+binary64@users.noreply.github.com>
2023-05-05 21:16:44 +0100

    test(app): update greeting selector to aria

--- apps/blabber-e2e/src/e2e/app.cy.ts
+++ apps/blabber-e2e/src/e2e/app.cy.ts
@@ -1,13 +1,19 @@
-import { getGreeting } from "../support/app.po";
+import { getGreeting } from '../support/app.po';
 
-describe("blabber", () => {
-  beforeEach(() => cy.visit("/"));
+describe('blabber', () => {
+  beforeEach(() => cy.visit('/'));
 
-  it("should display welcome message", () => {
+  it('should display welcome message', () => {
     // Custom command example, see `../support/commands.ts` file
-    cy.login("my-email@something.com", "myPassword");
+    cy.login('my-email@something.com', 'myPassword');
 
     // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains("Welcome blabber");
+    getGreeting()
+      .should('not.be.empty')
+      .invoke('text')
+      .then((e) => {
+        cy.log(e);
+        expect(Number(e)).to.be.gte(0);
+      });
   });
 });
