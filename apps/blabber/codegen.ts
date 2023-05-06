06a0ded0f77b7800b845e8094ac41287f2eb39df
binary64 <1680627+binary64@users.noreply.github.com>
2023-05-06 17:22:09 +0100

    feat(dx): near-operation-file codegen

--- apps/blabber/codegen.ts
+++ apps/blabber/codegen.ts
@@ -2,17 +2,21 @@ import type { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
   schema: '../../services/api/schema.json',
-  documents: './modules/**/*.graphql',
+  documents: '**/!(*.generated).{ts,tsx,graphql}',
   generates: {
-    'graphql-types.generated.ts': { plugins: ['typescript'] },
-    'modules/': {
+    'graphql-types.generated.ts': {
+      plugins: ['typescript'],
+    },
+    './': {
       preset: 'near-operation-file',
       presetConfig: {
         extension: '.generated.tsx',
-        baseTypesPath: '../graphql-types.generated.ts',
+        baseTypesPath: 'graphql-types.generated.ts',
       },
       plugins: ['typescript-operations', 'typescript-react-apollo'],
+      config: { withHooks: true },
     },
   },
 };
+
 export default config;
