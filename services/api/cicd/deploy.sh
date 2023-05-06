124b8720f66a7062d90c0a34af6831f335aa5a66
binary64 <1680627+binary64@users.noreply.github.com>
2023-05-07 00:13:21 +0100

    fix(ci): try inline token

--- services/api/cicd/deploy.sh
+++ services/api/cicd/deploy.sh
@@ -6,10 +6,7 @@ curl -L https://fly.io/install.sh | sh
 export FLYCTL_INSTALL="$HOME/.fly"
 export PATH="$FLYCTL_INSTALL/bin:$PATH"
 
-# Login to Fly
-fly login --token "$FLY_API_TOKEN"
-
 # Update the infra deployment according to fly.toml
-fly deploy
+fly --token "$FLY_API_TOKEN" deploy
 
 pnpm hasura --skip-update-check --no-color deploy --disable-interactive
