e16f46328649c28b91e81689ccfe183e37794151
binary64 <1680627+binary64@users.noreply.github.com>
2023-05-07 16:12:37 +0100

    fix(ci): :construction: fix args

--- services/api/cicd/deploy.sh
+++ services/api/cicd/deploy.sh
@@ -7,6 +7,6 @@ export FLYCTL_INSTALL="$HOME/.fly"
 export PATH="$FLYCTL_INSTALL/bin:$PATH"
 
 # Update the infra deployment according to fly.toml
-fly deploy --config services/api/cicd --access-token "$FLY_API_TOKEN" --auto-accept
+fly deploy --config services/api/cicd --access-token "$FLY_API_TOKEN" --auto-confirm --force-machines --remote-only
 
 pnpm hasura --skip-update-check --no-color deploy --disable-interactive
