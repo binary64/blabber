2ac7a56fad291f138e690a7f9a7141f438aff540
binary64 <1680627+binary64@users.noreply.github.com>
2023-05-11 20:21:22 +0100

    fix(ci): pass through all the vars

--- services/api/cicd/deploy.sh
+++ services/api/cicd/deploy.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
-set -ex
-
-pwd
+set -e
 
 # Ensure FLY_API_TOKEN env var exists
 if [ -z "$FLY_API_TOKEN" ]; then
   echo "FLY_API_TOKEN env var is not set"
+  exit 1
+fi
+
+# Ensure HASURA_GRAPHQL_ADMIN_SECRET env var exists
+if [ -z "$HASURA_GRAPHQL_ADMIN_SECRET" ]; then
+  echo "HASURA_GRAPHQL_ADMIN_SECRET env var is not set"
+  exit 1
+fi
+
+# Ensure HASURA_GRAPHQL_ENDPOINT env var exists
+if [ -z "$HASURA_GRAPHQL_ENDPOINT" ]; then
+  echo "HASURA_GRAPHQL_ENDPOINT env var is not set"
   exit 1
 fi
 
