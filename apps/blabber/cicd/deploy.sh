6e684ec3a29c85ee2a9604b9fa7ee7112fc100d9
binary64 <1680627+binary64@users.noreply.github.com>
2023-05-12 21:40:07 +0100

    feat(ci): add deploy target for apps/blabber

--- apps/blabber/cicd/deploy.sh
+++ apps/blabber/cicd/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -e
 
-mv dist/apps/blabber/.next dist/apps/blabber/next
+mv dist/apps/blabber/.next dist/apps/blabber/next || true
 pnpm dlx vercel --cwd dist/apps/blabber --token "$VERCEL_TOKEN" deploy
