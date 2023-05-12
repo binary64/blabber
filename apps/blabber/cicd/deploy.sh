38229ec08bb289932df94cdc510e74fbbf349495
binary64 <1680627+binary64@users.noreply.github.com>
2023-05-13 00:17:39 +0100

    refactor: remove .next juggling

--- apps/blabber/cicd/deploy.sh
+++ apps/blabber/cicd/deploy.sh
@@ -1,7 +1,20 @@
 #!/bin/sh
 set -e
 
-mv dist/apps/blabber/.next dist/apps/blabber/next || true
+echo "Ensuring Vercel token exists..."
+if [ -z "$VERCEL_TOKEN" ]; then
+  echo "VERCEL_TOKEN env var is not set"
+  exit 1
+fi
+
+echo "Installing Vercel CLI..."
 npm i -g vercel
+echo "Installed!"
+
+echo "Logging in..."
 vercel --no-color --cwd dist/apps/blabber --local-config=apps/blabber/cicd/vercel.json --token "$VERCEL_TOKEN" link --yes
+echo "Logged in!"
+
+echo "Deploying..."
 vercel --no-color --cwd dist/apps/blabber --local-config=apps/blabber/cicd/vercel.json --token "$VERCEL_TOKEN" deploy --yes --prod
+echo "Deployed!"
