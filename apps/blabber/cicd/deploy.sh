a36c6692be224dcf236c846c649489955d944242
binary64 <1680627+binary64@users.noreply.github.com>
2023-05-12 22:44:34 +0100

    fix(ci): vercel deploy with `link`

--- apps/blabber/cicd/deploy.sh
+++ apps/blabber/cicd/deploy.sh
@@ -2,4 +2,6 @@
 set -e
 
 mv dist/apps/blabber/.next dist/apps/blabber/next || true
-pnpx vercel --local-config=apps/blabber/cicd/vercel.json --token "$VERCEL_TOKEN" deploy dist/apps/blabber --prod
+npm i -g vercel
+vercel --no-color --cwd dist/apps/blabber --local-config=apps/blabber/cicd/vercel.json --token "$VERCEL_TOKEN" link --yes
+vercel --no-color --cwd dist/apps/blabber --local-config=apps/blabber/cicd/vercel.json --token "$VERCEL_TOKEN" deploy --yes --prod
