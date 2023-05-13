ad435f6f115af33b3409f4a7984aee7f3c8b2167
binary64 <1680627+binary64@users.noreply.github.com>
2023-05-13 19:48:45 +0100

    feat(web): add sentry to nextjs

--- apps/blabber/next.config.js
+++ apps/blabber/next.config.js
@@ -1,7 +1,5 @@
-//@ts-check
-
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const { withNx } = require('@nx/next/plugins/with-nx');
+const { composePlugins, withNx } = require('@nx/next');
+const { withSentryConfig } = require("@sentry/nextjs");
 
 /**
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
@@ -14,4 +12,37 @@ const nextConfig = {
   },
 };
 
-module.exports = withNx(nextConfig);
+const plugins = [
+  withNx,
+];
+
+module.exports = withSentryConfig(
+  composePlugins(...plugins)(nextConfig),
+  {
+    // For all available options, see:
+    // https://github.com/getsentry/sentry-webpack-plugin#options
+
+    // Suppresses source map uploading logs during build
+    silent: true,
+  },
+  {
+    // For all available options, see:
+    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+
+    // Upload a larger set of source maps for prettier stack traces (increases build time)
+    widenClientFileUpload: true,
+
+    // Transpiles SDK to be compatible with IE11 (increases bundle size)
+    transpileClientSDK: true,
+
+    // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
+    tunnelRoute: "/monitoring",
+
+    // Hides source maps from generated client bundles
+    hideSourceMaps: true,
+
+    // Automatically tree-shake Sentry logger statements to reduce bundle size
+    disableLogger: true,
+  }
+);
+
