a95baa4cb41ce5a8bd7609154e7573d06f296793
binary64 <1680627+binary64@users.noreply.github.com>
2023-07-16 13:38:56 +0100

    Revert "feat: next-auth"
    This reverts commit 1cc38a333aba29ac130272d9a5abf29dcf5d4d41.

--- apps/blabber/modules/user/index.tsx
+++ apps/blabber/modules/user/index.tsx
@@ -1,5 +1,4 @@
 import gql from 'graphql-tag';
-import { signIn, useSession } from 'next-auth/react';
 import styled from '@emotion/styled';
 import {
   NumberOfSiteUsersDocument,
@@ -27,7 +26,6 @@ const StyledPage = styled.div`
 `;
 
 export function Index() {
-  const { data: session } = useSession();
   const { data } = useNumberOfSiteUsersQuery();
   const [handleAction] = useMutation(
     gql`
@@ -65,22 +63,8 @@ export function Index() {
     throw new Error('Function not implemented.');
   }
 
-  if (!session) {
-    return (
-      <a
-        onClick={(e) => {
-          e.preventDefault();
-          signIn();
-        }}
-      >
-        You must be signed in to view this page
-      </a>
-    );
-  }
-
   return (
     <StyledPage>
-      <pre>{JSON.stringify(session, null, 2)}</pre>
       react:strict 👋{data?.user_aggregate.aggregate?.count}👋
       <button
         className="btn btn-wide btn-primary glass"
--- apps/blabber/pages/_app.tsx
+++ apps/blabber/pages/_app.tsx
@@ -1,32 +1,25 @@
-import type { AppProps } from 'next/app';
+import { AppProps } from 'next/app';
 import Head from 'next/head';
+import './styles.css';
 import { GraphqlProvider } from '../layouts/ApolloProvider';
 import { useEffect } from 'react';
 import { themeChange } from 'theme-change';
-import { SessionProvider } from 'next-auth/react';
-import type { Session } from 'next-auth';
-import './styles.css';
 
-function CustomApp({
-  Component,
-  pageProps: { session, ...pageProps },
-}: AppProps<{ session: Session }>) {
+function CustomApp({ Component, pageProps }: AppProps) {
   useEffect(() => {
     themeChange(false);
     // 👆 false parameter is required for react project
   }, []);
 
   return (
-    <SessionProvider session={session}>
-      <GraphqlProvider>
-        <Head>
-          <title>Welcome to blabber!</title>
-        </Head>
-        <main className="app">
-          <Component {...pageProps} />
-        </main>
-      </GraphqlProvider>
-    </SessionProvider>
+    <GraphqlProvider>
+      <Head>
+        <title>Welcome to blabber!</title>
+      </Head>
+      <main className="app">
+        <Component {...pageProps} />
+      </main>
+    </GraphqlProvider>
   );
 }
 
