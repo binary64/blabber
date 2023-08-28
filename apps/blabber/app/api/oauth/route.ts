b88e2977379fada488316843ef758d4a1ad96332
binary64 <1680627+binary64@users.noreply.github.com>
2023-08-28 23:07:47 +0100

    chore: minor refactor for /oauth route. make prod more secure with https only flag.

--- apps/blabber/app/api/oauth/route.ts
+++ apps/blabber/app/api/oauth/route.ts
@@ -6,14 +6,14 @@ export const GET = async (request: Request) => {
   const url = new URL(request.url);
   const provider = url.searchParams.get('provider');
   if (provider === 'google') {
-    const [authorizationUrl, state] =
-      await googleAuth.getAuthorizationUrl();
+    const [url, state] = await googleAuth.getAuthorizationUrl();
     cookies().set('oauth_state', state, {
       path: '/',
       maxAge: 60 * 60,
       httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
     });
-    return NextResponse.redirect(authorizationUrl.toString());
+    return NextResponse.redirect(url);
   }
   return new Response(null, {
     status: 400,
