4491f547177ce3753016d4a791a6feff017f736d
binary64 <1680627+binary64@users.noreply.github.com>
2023-08-29 00:22:52 +0100

    chore: update file based on example from lucia repo

--- apps/blabber/app/api/oauth/google/route.ts
+++ apps/blabber/app/api/oauth/google/route.ts
@@ -1,35 +1,67 @@
 import { auth, googleAuth } from '../../../../modules/user/lucia';
+import { OAuthRequestError } from '@lucia-auth/oauth';
 import { cookies } from 'next/headers';
-import { NextRequest, NextResponse } from 'next/server';
+
+import type { NextRequest } from 'next/server';
 
 export const GET = async (request: NextRequest) => {
-  const url = new URL(request.url);
-  const code = url.searchParams.get('code');
-  const state = url.searchParams.get('state');
-  const storedState = cookies().get('oauth_state')?.value ?? null;
-  if (!storedState || storedState !== state || !code || !state) {
-    return new Response(null, { status: 401 });
-  }
-  const {
-    existingUser,
-    googleUser: providerUser,
-    createUser,
-  } = await googleAuth.validateCallback(code);
-
-  const getUser = async () => {
-    if (existingUser) return existingUser;
-    return await createUser({
-      attributes: {
-        fullName: providerUser.name,
+  const authRequest = auth.handleRequest({ request, cookies });
+  const session = await authRequest.validate();
+  if (session) {
+    return new Response(null, {
+      status: 302,
+      headers: {
+        Location: '/',
       },
     });
-  };
-  const user = await getUser();
-  const session = await auth.createSession({
-    userId: user.userId,
-    attributes: {},
-  });
-  const authRequest = auth.handleRequest({ request, cookies });
-  authRequest.setSession(session);
-  return NextResponse.redirect(new URL('/', url));
+  }
+  const cookieStore = cookies();
+  const storedState = cookieStore.get('github_oauth_state')?.value;
+  const url = new URL(request.url);
+  const state = url.searchParams.get('state');
+  const code = url.searchParams.get('code');
+  // validate state
+  if (!storedState || !state || storedState !== state || !code) {
+    return new Response(null, {
+      status: 400,
+    });
+  }
+  try {
+    const { getExistingUser, googleUser, createUser } =
+      await googleAuth.validateCallback(code);
+
+    const getUser = async () => {
+      const existingUser = await getExistingUser();
+      if (existingUser) return existingUser;
+      const user = await createUser({
+        attributes: {
+          email: googleUser.email,
+        },
+      });
+      return user;
+    };
+
+    const user = await getUser();
+    const session = await auth.createSession({
+      userId: user.userId,
+      attributes: {},
+    });
+    authRequest.setSession(session);
+    return new Response(null, {
+      status: 302,
+      headers: {
+        Location: '/',
+      },
+    });
+  } catch (e) {
+    if (e instanceof OAuthRequestError) {
+      // invalid code
+      return new Response(null, {
+        status: 400,
+      });
+    }
+    return new Response(null, {
+      status: 500,
+    });
+  }
 };
