cc196064205b7dd314fcddc18c513db770620ae0
binary64 <1680627+binary64@users.noreply.github.com>
2023-10-25 19:38:18 +0100

    refactor: change page for rsc

--- apps/blabber/app/page.tsx
+++ apps/blabber/app/page.tsx
@@ -2,19 +2,8 @@
 
 'use server';
 
-import { auth } from '../modules/user/lucia';
-import { cookies } from 'next/headers';
-import { redirect } from 'next/navigation';
-import Index from '../modules/user';
-
 const Page = async () => {
-  const authRequest = auth.handleRequest({
-    cookies,
-    request: null,
-  });
-  const session = await authRequest.validate();
-  if (!session) redirect('/login');
-  return <Index />;
+  return <h1>hii</h1>;
 };
 
 export default Page;
