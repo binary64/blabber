5ef5fcb9019f4c88caf8417a8c7f4fcc859db759
binary64 <1680627+binary64@users.noreply.github.com>
2024-05-05 20:12:51 +0100

    force rebuild

--- apps/blabber/cicd/deploy.sh
+++ apps/blabber/cicd/deploy.sh
@@ -21,14 +21,11 @@ echo "Installed!"
 
 echo "Logging in..."
 vercel --no-color --cwd dist/apps/blabber --local-config=apps/blabber/cicd/vercel.json --token "$VERCEL_TOKEN" link --yes
-ls -al dist/apps/blabber
 vercel --no-color --cwd dist/apps/blabber --local-config=apps/blabber/cicd/vercel.json --token "$VERCEL_TOKEN" pull --yes
-ls -al dist/apps/blabber
 echo "Logged in!"
 
 echo "Building..."
 vercel --no-color --cwd dist/apps/blabber --local-config=apps/blabber/cicd/vercel.json --token "$VERCEL_TOKEN" build
-ls -al dist/apps/blabber
 echo "Built!"
 
 echo "Deploying..."
